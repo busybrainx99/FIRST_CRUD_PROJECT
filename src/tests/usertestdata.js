3641d668f3b6c7eb9f1f9c651ea5c5a6493c2d27
Goodness E. Eboh <ebohgoodness.t@gmail.com>
2023-08-08 10:07:30 +0100

    Changed usertestdata

--- src/tests/usertestdata.js
+++ src/tests/usertestdata.js
@@ -1,32 +1,32 @@
 const user = {
-    email: "randommail@gmail.com",
-    username: "Yusuf",
-    password: "123456"
+    email:"randommail@gmail.com",
+    username:"yusuf",
+    password:"123456"
 };
 
 const user2 = {
-    email: "randommail@gmail.com",
-    username: "Brenda",
-    password: "1234556"
+    email:"temporalmail@gmail.com",
+    username:"Brenda",
+    password:"12345567"
 };
 
-const user3 = {
-    email: "Garry@gmail.com",
-    username: "garry",
-    password: "123456"
+const user3 ={
+    email:"mainmail@gmail.com",
+    username:"mainman",
+    password:"12345678"
 }
 
 const user4 = {
-    email: "Temporalmail@gmail.com",
-    username: "Yusuf",
-    password: "1234556"
+    email:"temporalmail@gmail.com",
+    username:"Brenda",
+    password:"1234556789"
 };
 
 const profile = {
-    firstName: "Ibrahim",
-    lastName: "Mohammed",
-    phoneNumber:"+234707843456"
-};
+    firstName: "Joseph",
+    lastName: "King",
+    phoneNumber:"+2348109856487"
+}
 
 export {
     user,
@@ -35,73 +35,3 @@ export {
     user4,
     profile
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const user = {
-//     email:"randommail@gmail.com",
-//     username:"yusuf",
-//     password:"123456"
-// };
-
-// const user2 = {
-//     email:"temporalmail@gmail.com",
-//     username:"Brenda",
-//     password:"12345567"
-// };
-
-// const user3 ={
-//     email:"mainmail@gmail.com",
-//     username:"mainman",
-//     password:"12345678"
-// }
-
-// const user4 = {
-//     email:"temporalmail@gmail.com",
-//     username:"Brenda",
-//     password:"1234556789"
-// };
-
-// const profile = {
-//     firstName: "Joseph",
-//     lastName: "King",
-//     phoneNumber:"+2348109856487"
-// }
-
-// export {
-//     user,
-//     user2,
-//     user3,
-//     user4,
-//     profile
-// };
